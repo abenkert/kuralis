f1054f0c962a87e667be7ec97b947de290a67ce0
abenkert <andrew.benkert@yahoo.com>
2025-03-28 10:36:26 -0400

    Fix copy address on orders page

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,13 +6,13 @@ Sidekiq.configure_server do |config|
   config.queues = [ "default", "images" ]
 
   # Load the schedule
-  schedule_file = File.expand_path("../../sidekiq.yml", __FILE__)
-  if File.exist?(schedule_file)
-    schedule = YAML.load_file(schedule_file)
-    Sidekiq::Scheduler.enabled = true
-    Sidekiq::Scheduler.dynamic = true
-    Sidekiq.schedule = schedule[:scheduler][:schedule]
-  end
+  # schedule_file = File.expand_path("../../sidekiq.yml", __FILE__)
+  # if File.exist?(schedule_file)
+  #   schedule = YAML.load_file(schedule_file)
+  #   Sidekiq::Scheduler.enabled = true
+  #   Sidekiq::Scheduler.dynamic = true
+  #   Sidekiq.schedule = schedule[:scheduler][:schedule]
+  # end
   # config.periodic do |mgr|
   #   mgr.register('*/5 * * * *', 'FetchEbayOrdersJob', retry: false)
   #   mgr.register('*/5 * * * *', 'FetchShopifyOrdersJob', retry: false)
